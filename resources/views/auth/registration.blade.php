2dc4e1b78bcdb6d5a392a44a7d148a784ab250ea
cyutht3t.3 <cyutht3t.30@gmail.com>
2023-10-20 09:54:18 +0300

    return dropzone

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -51,11 +51,22 @@
             </div>
           </div>
         </div>
-        <div class="registration__input-file-container">
-          <input type="file" name="avatar">
-          @error('avatar')
-            <div style="color:red; margin-top:7px;">{{ $message }}</div>
-          @enderror 
+        <div class="registration__input-file-wrapper form__input-file-wrapper">
+          <div class="registration__file-zone form__file-zone dropzone">
+            <input class="registration__input-file form__input-file" id="userpic-file" type="file" name="avatar" title=" ">
+            <div class="form__file-zone-text">
+              <span>Перетащите фото сюда</span>
+            </div>
+          </div>
+          <button class="registration__input-file-button form__input-file-button button" type="button">
+            <span>Выбрать фото</span>
+            <svg class="registration__attach-icon form__attach-icon" width="10" height="20">
+              <use xlink:href="#icon-attach"></use>
+            </svg>
+          </button>
+        </div>
+        <div class="registration__file form__file dropzone-previews">
+
         </div>
         <button class="registration__submit button button--main" type="submit">Отправить</button>
       </form>
